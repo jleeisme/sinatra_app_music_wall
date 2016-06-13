cf3e813e03c5ec56ea25fe9eef8954b23fc29f74
Jason Lee <jleeisme@gmail.com>
2016-06-13 19:51:09 +0000

    added song.rb, index.erb, and migration for add_songs.rb

---
+++ db/migrate/20160613185248_add_songs.rb
@@ -0,0 +1,11 @@
+class AddSongs < ActiveRecord::Migration
+  def change
+    create_table :songs do |t|
+      t.string :title
+      t.string :author
+      t.string :url
+      t.timestamps :created_at
+      t.timestamps :updated_at
+    end
+  end
+end
