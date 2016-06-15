5047a5a88074d50ae3f9d7bf6f7a6eb8244e7a7c
Jason Lee <jleeisme@gmail.com>
2016-06-15 15:49:54 +0000

    added login, signup, logout and their forms

--- app/actions.rb
+++ app/actions.rb
@@ -1,22 +1,21 @@
 # Homepage (Root path)
-# 
-# enable :sessions...already enabled in environment
-
-# need a helper to verify the user instead of looping through each time
-# called within the gets
 helpers do
-  def login?
-    if session[:username].nil?
-      redirect '/login'
-    else
-      return true
+  def enforce_login
+    unless session[:username]
+      redirect '/login' 
     end
   end
+  # def login?
+  #   if session[:username].nil?
+  #     redirect '/login'
+  #   else
+  #    true
+  #   end
+  # end
 
-  def username
-    return session[:username]
-  end
-
+  # def username
+  #   return session[:username]
+  # end
 end
 
 get '/' do
@@ -26,7 +25,7 @@ end
 # search username and password to match the @user
 # session will match the username
 post '/' do
-  @user = User.where(username: params[:username],
+  user = User.where(username: params[:username],
     password: params[:password])
     session[:username] = @user
 end
@@ -40,33 +39,54 @@ get '/logout' do
 end
 
 # needs the form inputed
-get '/songs/signup' do
+get '/signup' do
+  current_user
   erb :'songs/signup'
 end
 
 # new user has a username and password which is saved
 # redirected to songs where they can then post songs etc.
-post '/songs/signup' do
-  @user = User.new(
-    username: params[:username],
-    password: params[:password]
-  )
-    @user.save
-    redirect '/songs'
+post '/signup' do
+  user = User.new
+  user.username = params[:username]
+  user.password = params[:password]
+  user.save!
+  redirect '/songs'
 end
 
+get '/login' do
+  if session[:username]
+    redirect '/songs'
+  end
+  erb :'index'
+end
+
+post '/login' do
+  user = User.find_by(username: params[:username])
+  unless user
+    redirect '/login'
+  end
+  if user.password == params[:password]
+    session[:username] = user
+    redirect '/songs'
+  else
+    redirect '/login'
+  end
+end
 # if on the session the username isn't nil, songs is ok
 # else redirected to index
 get '/songs' do
-  @songs = Song.all
-  # if !session[:username].nil?
-    erb :'songs/index'
-  # else
-  #   redirect '/'
+  enforce_login
+  # unless session[:username]
+  #   redirect '/login'
   # end
+  @song = Song.all
+  # if !session[:username].nil?
+  erb :'songs/index'
 end
 
 get '/songs/new' do
+  current_user
   # login?
   erb :'songs/new'
 end
@@ -78,6 +98,11 @@ post '/songs/new' do
     author: params[:author],
     url:    params[:url]
   )
-  @song.save
+  @song.save!
   redirect '/songs'
+end
+
+get '/songs/:id' do
+  @song = Song.find params[:id]
+  erb :'songs/show'
 end
